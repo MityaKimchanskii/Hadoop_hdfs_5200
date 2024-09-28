367d245661db3c037c5cea12d73377298bd49889
Dmitrii Kim <mitya_kim@icloud.com>
2024-09-28 09:01:04 -0700

    Add Lab2

--- 5200_prepare_for_exam.sql
+++ 5200_prepare_for_exam.sql
@@ -25,13 +25,13 @@ pwd
 echo "Big Data"
 
 -- You can use “echo” and “>” if you want to add some words to a file named test.txt. And, you can use
--- “cat” to display the content of the file:
+    -- “cat” to display the content of the file:
 echo "Adding first line" > test.txt
 cat test.txt
 
 -- If you want to COPY a file named test.txt from your Big Data server, to your PC, you have to run
---  another git bash terminal without runnung ssh.
--- dot at the end of the command is REQUIRED!!!
+    --  another git bash terminal without runnung ssh.
+    -- dot at the end of the command is REQUIRED!!!
 scp dkim171@129.146.230.230:/home/dkim171/test.txt .
 -- to find file local on your computer enter the command
 ls -al test.txt
@@ -43,10 +43,10 @@ exit
 --/--------------------------------------Lab2-----------------------------------------/-- 
 --/-----------------------------------------------------------------------------------/-- 
 -- In this lab You will analyze and visualize sensor data. Thus,
--- • You should learn how to download sensor data to the local systems in Amazon AWS Cloud.
--- • Then, you will learn how to upload it to HDFS.
--- • You will figure out how to manipulate and analyze sensor data in HDFS using HiveQL.
--- • You will also practice how to visualize the result in Excel.
+    -- • You should learn how to download sensor data to the local systems in Amazon AWS Cloud.
+    -- • Then, you will learn how to upload it to HDFS.
+    -- • You will figure out how to manipulate and analyze sensor data in HDFS using HiveQL.
+    -- • You will also practice how to visualize the result in Excel.
 
 -- 1. connect to the Big Data Server 
 ssh dkim171@129.146.230.230
@@ -77,12 +77,12 @@ hdfs dfs -ls SensorFiles/hvac -- list files
 hdfs dfs -ls SensorFiles/building -- list files
 
 -- 6. Creating Hive table to Query Sensor data
--- The following Hive statement creates an external table that allows Hive to query data stored in
--- HDFS. External tables preserve the data in the original file format, while allowing Hive to perform
--- queries against the data within the file.
--- The Hive statements below create two new tables, named hvac and building, by describing the
--- fields within the files, the delimiter (comma) between fields, and the location of the file in Azure
--- Blob Storage. This will allow you to create Hive queries over your data.
+    -- The following Hive statement creates an external table that allows Hive to query data stored in
+    -- HDFS. External tables preserve the data in the original file format, while allowing Hive to perform
+    -- queries against the data within the file.
+    -- The Hive statements below create two new tables, named hvac and building, by describing the
+    -- fields within the files, the delimiter (comma) between fields, and the location of the file in Azure
+    -- Blob Storage. This will allow you to create Hive queries over your data.
 
 -- 7. Run the following HDFS command to make beeline command works
 hdfs dfs -chmod -R o+w . -- 
@@ -96,17 +96,20 @@ use dkim171; -- to use my database
 
 -- 9. HiveQL code to create an external table “hvac”.
 --/-----------------------------------------------------------------------------------/--
+--/-------------------------------------CODE SQL--------------------------------------/--
 DROP TABLE IF EXISTS hvac;
 -- create the hvac table on comma-separated sensor data
-CREATE EXTERNAL TABLE IF NOT EXISTS hvac(`date` STRING, `time` STRING,
-targettemp BIGINT,
-actualtemp BIGINT,
-system BIGINT,
-systemage BIGINT,
-buildingid BIGINT)
+CREATE EXTERNAL TABLE IF NOT EXISTS hvac(`date`     STRING, 
+                                        `time`      STRING, 
+                                        targettemp  BIGINT, 
+                                        actualtemp  BIGINT, 
+                                        system      BIGINT,
+                                        systemage   BIGINT,
+                                        buildingid  BIGINT)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/hvac'
 TBLPROPERTIES ('skip.header.line.count'='1');
+--/----------------------------------------- END -------------------------------------/--
 --/-----------------------------------------------------------------------------------/--
 show tables; -- list all tables in my database
 select * from hvac limit 10; --  query the content of the hvac table
@@ -114,138 +117,97 @@ describe formatted hvac; -- to see and check the description of the table
 
 -- 10. HiveQL code to create another external table “building”.
 --/-----------------------------------------------------------------------------------/--
+--/-------------------------------------CODE SQL--------------------------------------/--
 DROP TABLE IF EXISTS building;
 --create the building table on comma-separated building data
-CREATE EXTERNAL TABLE IF NOT EXISTS building(buildingid BIGINT, buildingmgr
-STRING,
-buildingage BIGINT,
-hvacproduct STRING,
-country STRING)
+CREATE EXTERNAL TABLE IF NOT EXISTS building(buildingid BIGINT, 
+                                             buildingmgr STRING,
+                                             buildingage BIGINT,
+                                             hvacproduct STRING,
+                                             country STRING)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/building'
 TBLPROPERTIES ('skip.header.line.count'='1');
+--/----------------------------------------- END -------------------------------------/--
 --/-----------------------------------------------------------------------------------/--
 show tables; -- to check if building table created successfully
 select * from building limit 10; -- select 10 rows from the table building and check
+describe formatted building; -- building table information
 
+--/-----------------------------------------------------------------------------------/--
+-- 11. The following Hive queries create select temperatures from your HVAC data, looking for
+    -- temperature variations (see the query below). Specifically, the difference between the target
+    -- temperature the thermostat was set to and the recorded temperature. If the difference is greater
+    -- than 5, the temp_diff column will be set to 'HOT',or 'COLD' and extremetemp will be set to 1;
+    -- otherwise, temp_diff will be set to ‘NORMAL’ and extremetemp will be set to 0.
+    -- The queries will write the results into two new tables: hvac_temperatures and hvac_building (see
+    -- the CREATE TABLE statements below). The hvac_building table will contain building information
+    -- such as the manager, building age, and the HVAC system for buildings, and will also be used to look
+    -- up temperature data for the building through the JOIN with the hvac_temperatures table.
+--/-----------------------------------------------------------------------------------/--
+--/-------------------------------------CODE SQL--------------------------------------/--
+DROP TABLE IF EXISTS hvac_temperatures;
+--create the hvac_temperatures table by selecting from the hvac table
+CREATE TABLE hvac_temperatures
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/hvac_temperatures/'
+AS
+SELECT *, targettemp - actualtemp AS temp_diff,
+IF((targettemp - actualtemp) > 5, 'COLD',
+IF((targettemp - actualtemp) < -5, 'HOT', 'NORMAL')) AS temprange,
+IF((targettemp - actualtemp) > 5, '1', 
+IF((targettemp - actualtemp) < -5, '1', 0)) AS extremetemp
+FROM hvac;
+--/----------------------------------------- END -------------------------------------/--
+--/-----------------------------------------------------------------------------------/--
+show tables; -- to check if building table created successfully
+select * from building limit 10; -- select 10 rows from the table building and check
+describe formatted building; -- building table information
+select targettemp, actualtemp, system, systemage, temp_diff, temprange, extremetemp from hvac_temperatures LIMIT 10;
 
+--/-----------------------------------------------------------------------------------/--
+--/-------------------------------------CODE SQL--------------------------------------/--
+DROP TABLE IF EXISTS hvac_building;
+--create the hvac_building table by joining the building table and the hvac_temperatures table
+CREATE TABLE hvac_building
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/hvac_building/'
+AS SELECT h.*, b.country, b.hvacproduct, b.buildingage, b.buildingmgr
+FROM building b JOIN hvac_temperatures h ON b.buildingid = h.buildingid;
+--/----------------------------------------- END -------------------------------------/--
+--/-----------------------------------------------------------------------------------/--
+show tables; -- to check if building table created successfully
 
+select targettemp, actualtemp, system, systemage, temp_diff,temprange, extremetemp, country, hvacproduct, buildingmgr 
+from hvac_building LIMIT 10;; -- select 10 rows from the table building and check
 
+describe formatted hvac_building; -- building table information
 
-JMv8bkUuB@JUq@9
+-- 12. To get out of beeline 
+!exit
 
- cat 000000_0 000001_0 > final_0
-
-
+-- 13. Open another terminal with git bash, minty, Windows 10 cmd CLI, or putty, which is to connect
+    -- the BIG DATA CLOUD to download the output file 000000_0 at the HDFS path
+    -- “/user/dkim171/SensorFiles/hvac_building ”:
 ssh dkim171@129.146.230.230
-ls -al
-hdfs dfs -ls
+hdfs dfs -ls SensorFiles/hvac_building
 
-ls SensorFiles
+hdfs dfs -get SensorFiles/hvac_building/000000_0 -- Download the file, 000000_0, to the local file systems
+hdfs dfs -get SensorFiles/hvac_building/000001_0 -- Download the file, 000000_0, to the local file systems
+ls -al -- check if the file was downloaded successfully
+cat 000000_0 000001_0 > final_0 -- create new merged file with all data from both files
 
+-- 14. Open another terminal with git bash, minty, or putty in order to read/import the output file
+    -- using your lab computer (or your PC/Laptop) - you have to download the file to your lab
+    -- computer (or your PC/Laptop). 
+scp dkim171@129.146.230.230:/home/dkim171/final_1 downloaded_final_1.csv
 
-CREATE DATABASE if not exists dkim171;
-show DATABASES
+--/----------------------------The end of the Lab 2-----------------------------------/-- 
 
+--/-----------------------------------------------------------------------------------/-- 
+--/--------------------------------------Lab3-----------------------------------------/-- 
+--/-----------------------------------------------------------------------------------/-- 
 
- dfs –ls
-
-
-DROP TABLE IF EXISTS building;
---create the building table on comma-separated building data
-CREATE EXTERNAL TABLE IF NOT EXISTS building(buildingid BIGINT, buildingmgr
-STRING,
-    buildingage BIGINT,
-    hvacproduct STRING,
-    country STRING)
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/building'
-TBLPROPERTIES ('skip.header.line.count'='1');
-
-https://bit.ly/30SD9NG
-
-
-
-DROP TABLE IF EXISTS hvac;
---create the hvac table on comma-separated sensor data
-CREATE EXTERNAL TABLE IF NOT EXISTS hvac(`date` STRING, `time` STRING,
-targettemp BIGINT,
-    actualtemp BIGINT,
-    system BIGINT,
-    systemage BIGINT,
-    buildingid BIGINT)
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/hvac'
-TBLPROPERTIES ('skip.header.line.count'='1');
-DROP TABLE IF EXISTS hvac_temperatures;
---create the hvac_temperatures table by selecting from the hvac table
-CREATE TABLE hvac_temperatures
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/hvac_temperatures/'
-AS
-SELECT *, targettemp - actualtemp AS temp_diff,
-IF((targettemp - actualtemp) > 5, 'COLD',
-IF((targettemp - actualtemp) < -5, 'HOT', 'NORMAL')) AS temprange,
-IF((targettemp - actualtemp) > 5, '1', IF((targettemp - actualtemp) < -5,
-'1', 0)) AS extremetemp
-FROM hvac;
-
-
-DROP TABLE IF EXISTS hvac_building;
---create the hvac_building table by joining the building table and the
-hvac_temperatures table
-CREATE TABLE hvac_building
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/hvac_building/'
-AS
-SELECT h.*, b.country, b.hvacproduct, b.buildingage, b.buildingmgr
-FROM building b JOIN hvac_temperatures h ON b.buildingid = h.buildingid;
-
-
-
-
-
-
-DROP TABLE IF EXISTS hvac_temperatures;
---create the hvac_temperatures table by selecting from the hvac table
-CREATE TABLE hvac_temperatures
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/hvac_temperatures/'
-AS
-SELECT *, targettemp - actualtemp AS temp_diff,
-IF((targettemp - actualtemp) > 5, 'COLD',
-IF((targettemp - actualtemp) < -5, 'HOT', 'NORMAL')) AS temprange,
-IF((targettemp - actualtemp) > 5, '1', IF((targettemp - actualtemp) < -5,
-'1', 0)) AS extremetemp
-FROM hvac;
-
-DROP TABLE IF EXISTS hvac_building;
---create the hvac_building table by joining the building table and the
-hvac_temperatures table
-CREATE TABLE hvac_building
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/hvac_building/'
-AS
-SELECT h.*, b.country, b.hvacproduct, b.buildingage, b.buildingmgr
-FROM building b JOIN hvac_temperatures h ON b.buildingid = h.buildingid;
-
-
-DROP TABLE IF EXISTS hvac_building;
-
-CREATE TABLE hvac_building
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/hvac_building/'
-AS
-SELECT h.*, b.country, b.hvacproduct, b.buildingage, b.buildingmgr
-FROM building b JOIN hvac_temperatures h ON b.buildingid = h.buildingid;
-
-scp dkim171@129.146.230.230:/home/dkim171/000000_0 000000_0.csv
-
--bash-4.1$ hdfs dfs -get SensorFiles/hvac_building/000000_0
-
-scp dkim171@129.146.230.230:/Users/mityakim/Desktop/lab_2_5200/000000_0 000000_0.csv
-
-/Users/mityakim/Desktop/lab_2_5200/
 
 
 // lab 3
