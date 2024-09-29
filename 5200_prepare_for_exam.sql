77c12992254f1930a7da3ca44bd7623e1a791e93
Dmitrii Kim <mitya_kim@icloud.com>
2024-09-29 11:47:12 -0700

    Add lab3

--- 5200_prepare_for_exam.sql
+++ 5200_prepare_for_exam.sql
@@ -90,7 +90,7 @@ hdfs dfs -ls -- list files on HDFS
 beeline -- Open hive CLI (Command Line Shell Interface)
 
 -- 8. to create your database with your username to separate your tables with other users
-CREATE DATABASE if not exists dkim171
+CREATE DATABASE if not exists dkim171; -- create database dkim171
 show DATABASES; -- list all databases
 use dkim171; -- to use my database
 
@@ -109,7 +109,7 @@ CREATE EXTERNAL TABLE IF NOT EXISTS hvac(`date`     STRING,
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/hvac'
 TBLPROPERTIES ('skip.header.line.count'='1');
---/----------------------------------------- END -------------------------------------/--
+--/---------------------------------End CODE SQL--------------------------------------/--
 --/-----------------------------------------------------------------------------------/--
 show tables; -- list all tables in my database
 select * from hvac limit 10; --  query the content of the hvac table
@@ -128,7 +128,7 @@ CREATE EXTERNAL TABLE IF NOT EXISTS building(buildingid BIGINT,
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE LOCATION '/user/dkim171/SensorFiles/building'
 TBLPROPERTIES ('skip.header.line.count'='1');
---/----------------------------------------- END -------------------------------------/--
+--/---------------------------------End CODE SQL--------------------------------------/--
 --/-----------------------------------------------------------------------------------/--
 show tables; -- to check if building table created successfully
 select * from building limit 10; -- select 10 rows from the table building and check
@@ -158,7 +158,7 @@ IF((targettemp - actualtemp) < -5, 'HOT', 'NORMAL')) AS temprange,
 IF((targettemp - actualtemp) > 5, '1', 
 IF((targettemp - actualtemp) < -5, '1', 0)) AS extremetemp
 FROM hvac;
---/----------------------------------------- END -------------------------------------/--
+--/---------------------------------End CODE SQL--------------------------------------/--
 --/-----------------------------------------------------------------------------------/--
 show tables; -- to check if building table created successfully
 select * from building limit 10; -- select 10 rows from the table building and check
@@ -198,56 +198,89 @@ ls -al -- check if the file was downloaded successfully
 cat 000000_0 000001_0 > final_0 -- create new merged file with all data from both files
 
 -- 14. Open another terminal with git bash, minty, or putty in order to read/import the output file
-    -- using your lab computer (or your PC/Laptop) - you have to download the file to your lab
-    -- computer (or your PC/Laptop). 
-scp dkim171@129.146.230.230:/home/dkim171/final_1 downloaded_final_1.csv
+    -- using your lab computer (or your PC/Laptop) - you have to DOWNLOAD the file to your lab computer (or your PC/Laptop). 
+scp dkim171@129.146.230.230:/home/dkim171/final_1 downloaded_final_1.csv .
 
 --/----------------------------The end of the Lab 2-----------------------------------/-- 
 
 --/-----------------------------------------------------------------------------------/-- 
 --/--------------------------------------Lab3-----------------------------------------/-- 
 --/-----------------------------------------------------------------------------------/-- 
+-- In this sample you will use an Hive query that analyzes website log files to get insight into how
+    -- customers use the website. With this analysis, you can see the frequency of visits to the website in a day
+    -- from external websites, and a summary of website errors that the users experience.
+    -- In this tutorial, you'll learn how to use APACHE HADOOP to:
+        -- • Download website log files
+        -- • Create Hive tables to query those logs
+        -- • Create Hive queries to analyze the data
+        -- • Use Microsoft Excel to connect to APACHE Hadoop (using an ODBC connection) to retrieve the analyzed data
 
+-- 1. The ssh command to connect to the Hadoop Spark cluster. 
+ssh dkim171@129.146.230.230
 
-
-// lab 3
+-- 2. download the data file Website Log Data (909f2b.log) from a github:
 wget -O 909f2b.log https://github.com/dalgual/aidatasci/raw/master/data/bigdata/909f2b.log
 
+-- 3. to upload “909f2b.log” file to SampleLog directory of HDFS:
 hdfs dfs -mkdir SampleLog
 hdfs dfs -ls
 hdfs dfs -put 909f2b.log SampleLog/
 hdfs dfs -ls SampleLog/
 
+-- 4. open beeline
+beeline
+CREATE DATABASE if not exists dkim171;
+show DATABASES; -- list all databases
+use dkim171; -- to use my database
+-- or 
 create database dkim171;
-use dkim171;
-show databases;
+use dkim171; -- to use my database
 
+-- 5. create table weblogs
+--/-------------------------------------CODE SQL--------------------------------------/--
 DROP TABLE IF EXISTS weblogs;
 --create table weblogs on space-delimited website log data
-CREATE EXTERNAL TABLE IF NOT EXISTS weblogs(s_date date, s_time
-string, s_sitename string, cs_method string, cs_uristem string,
-cs_uriquery string, s_port int, cs_username string, c_ip
-string, cs_useragent string,
-cs_cookie string, cs_referer string, cs_host string, sc_status
-int, sc_substatus int,
-sc_win32status int, sc_bytes int, cs_bytes int, s_timetaken
-int )
+CREATE EXTERNAL TABLE IF NOT EXISTS weblogs(
+    s_date date, 
+    s_time string, 
+    s_sitename string, 
+    cs_method string, 
+    cs_uristem string,
+    cs_uriquery string, 
+    s_port int, 
+    cs_username string, 
+    c_ip string, 
+    cs_useragent string, 
+    cs_cookie string, 
+    cs_referer string, 
+    cs_host string, 
+    sc_status int, 
+    sc_substatus int, 
+    sc_win32status int, 
+    sc_bytes int, cs_bytes int, 
+    s_timetaken int)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ' '
 STORED AS TEXTFILE LOCATION '/user/dkim171/SampleLog/'
 TBLPROPERTIES ('skip.header.line.count'='2');
+--/---------------------------------End CODE SQL--------------------------------------/--
+-- 6. check new created table
+show tables;
 
-
-describe formatted weblogs;
-
+-- 7. check table select 10 rows
 select * from weblogs LIMIT 10;
 
+-- 8. describe weblogs table
+describe formatted weblogs;
 
-
-
+-- 9. create new table clientErrors
+--/-------------------------------------CODE SQL--------------------------------------/--
 DROP TABLE IF EXISTS ClientErrors;
 --create table ClientErrors for storing errors users experienced
-CREATE EXTERNAL TABLE ClientErrors(sc_status int, cs_referer string,
-cs_page string, cnt int)
+CREATE EXTERNAL TABLE ClientErrors(
+    sc_status int, 
+    cs_referer string, 
+    cs_page string, 
+    cnt int)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE LOCATION '/user/dkim171/ClientErrors/' ;
 --populate table ClientErrors with data from table weblogs
@@ -258,21 +291,29 @@ cs_page,
 count(distinct c_ip) as cnt
 FROM weblogs
 WHERE sc_status >=400 and sc_status < 500
-GROUP BY sc_status, cs_referer, concat(cs_uristem,'?',
-regexp_replace(cs_uriquery,'X-ARR-LOG-ID=[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}',''))
+GROUP BY sc_status, cs_referer, 
+concat(cs_uristem,'?', regexp_replace(cs_uriquery,'X-ARR-LOG-ID=[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}',''))
 ORDER BY cnt;
+--/---------------------------------End CODE SQL--------------------------------------/--
+show tables; -- check new table
+select * from ClientErrors ORDER BY CNT DESC LIMIT 10; -- select 10 rows from clienterrors
+-- 10. describe ClientErrors table
+describe formatted ClientErrors;
 
-describe formatted clienterrors;
-
-select * from clienterrors ORDER BY CNT DESC LIMIT 10;
-
-
-
-
+-- 11. The query for refersperday extracts data from the weblogs table for all external websites referencing
+    -- this website. The external website information is extracted from the cs_referer column of weblogs table.
+    -- To make sure the referring links did not encounter an error, the table only shows data for pages that
+    -- returned an HTTP status code between 200 and 300. The extracted data is then written to
+    -- the refersperday table.
+--/-------------------------------------CODE SQL--------------------------------------/--
 DROP TABLE IF EXISTS RefersPerDay;
 --create table RefersPerDay for storing references from external URL
-CREATE EXTERNAL TABLE IF NOT EXISTS RefersPerDay(year int, month int,
-day int, cs_referer string, cnt int)
+CREATE EXTERNAL TABLE IF NOT EXISTS RefersPerDay(
+    year int, 
+    month int,
+    day int, 
+    cs_referer string, 
+    cnt int)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE LOCATION '/user/dkim171/RefersPerDay/';
 --populate table RefersPerDay with data from the weblogs table
@@ -283,11 +324,22 @@ FROM weblogs
 WHERE sc_status >=200 and sc_status <300
 GROUP BY s_date, cs_referer
 ORDER BY cnt desc;
+--/---------------------------------End CODE SQL--------------------------------------/--
+show tables; -- check new table
+select * from RefersPerDay ORDER BY CNT DESC LIMIT 10; -- select 10 rows from RefersPerDay
+select year, month, cs_referer from refersperday LIMIT 10; -- select 10 rows from RefersPerDay with year and month, cs_referer
+-- 12. describe RefersPerDay table
+describe formatted RefersPerDay;
 
-hdfs dfs -ls /user/dkim171/SampleLog/
+-- 13. run the following hdfs command at the /user/jwoo5/SampleLog:
+    -- local linux terminal to find out weblogs file
+hdfs dfs -ls /user/dkim171/SampleLog/ -- It will show the file 909f2b.log as a result. At your local linux terminal, run the following hdfs command:
+hdfs dfs -ls /user/dkim171/RefersPerDay/ -- It will list 000000_0 file.
 
-hdfs dfs -ls /user/dkim171/RefersPerDay/
-
+-- 14. Download the hdfs file of the result to your local and copy it to your lab computer; use your account name not dkim171:
 hdfs dfs -get /user/dkim171/RefersPerDay/000000_0 log_result.out
-
+cat log_result.out
+-- 15. download or copy the output file to computer:
 scp dkim171@129.146.230.230:/home/dkim171/log_result.out .
+--/----------------------------The end of the Lab 3-----------------------------------/-- 
+
